8a2a655092bc30b6e2d922a4abc6269aca7c2574
김건우 <97003960+0o0o0o1@users.noreply.github.com>
2025-12-31 22:18:24 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,6 +13,19 @@ self.addEventListener("install", event => {
     })
   );
 });
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
 
 self.addEventListener("fetch", event => {
   event.respondWith(
