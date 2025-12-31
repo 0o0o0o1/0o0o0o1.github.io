436baf4dfccb52b9fbb42c53af95c47ce0b7b7ad
김건우 <97003960+0o0o0o1@users.noreply.github.com>
2025-12-31 22:20:30 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,18 @@
-const CACHE_NAME = "keen-cache-v1";
+const CACHE_NAME = "guard-cache-v1";
+
+/* 캐시할 파일 목록 */
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js"
+  "/keen-web/",
+  "/keen-web/index.html",
+  "/keen-web/style.css",
+
+  "/keen-web/rule.html",
+  "/keen-web/terms.html",
+  "/keen-web/calendar.html",
+  "/keen-web/team.html"
 ];
 
+/* 설치 단계 */
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -13,20 +20,23 @@ self.addEventListener("install", event => {
     })
   );
 });
+
+/* 🔥 활성화 단계: 이전 캐시 전부 삭제 */
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
           }
         })
-      )
-    )
+      );
+    })
   );
 });
 
+/* 요청 가로채기 */
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
